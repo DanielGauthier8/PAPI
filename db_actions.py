aea33bed456373e1fb6c28d36d181aff2bb8df76
DanielGauthier8 <dangauthier8@gmail.com>
2020-06-01 13:40:59 -0400

    Debugging has been separated

--- db_actions.py
+++ db_actions.py
@@ -525,37 +525,3 @@ def all_data(db_file, file_namez):
     file_dat["Large Text Insertion Detection*"] = large_insertion_check(the_pulse)
 
     return file_dat
-
-
-# ----------------------------------------Debugging
-
-
-def test():
-    cursor = set_cursor("./databases/jgoldman2468.db")
-    cursor = clean_up(cursor)
-    files_list = all_files(cursor)
-    # print(files_list)
-
-    # the_timeline, the_pulse = gather_many(cursor, files_list)
-    # deletions, insertions, deletions_list, insertions_list = deletions_insertions(the_timeline, the_pulse)
-    # print(len(deletions_list), len(insertions_list), len(the_pulse))
-    # fig = plt.figure()
-    # ax = plt.subplot(111)
-    # ax.plot(the_timeline, insertions_list, label='Insertions Over Time')
-    # plt.title('Insertions Over Time')
-    # ax.legend()
-    # plt.show()
-    # print(large_insertion_check(the_pulse))
-    # doc_file = documentz_info(cursor, files_list, "save_points")
-    # print(doc_file)
-    # deletions, insertions = deletions_insertions(cursor, files_list)
-    # print(str(deletions) + " " + str(insertions))
-
-    # general_pulse = gather(cursor, the_file)
-    # print(general_pulse)
-    #
-    # print(data_pulse)
-
-    return 0
-
-# test()

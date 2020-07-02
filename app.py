923f1ae1244c814554fec2a64b65e8c08913b866
Benjamin Dahrooge <benjamin@dahrooge.com>
2020-07-02 00:46:35 -0400

    implement bounds for class mode

--- app.py
+++ app.py
@@ -124,12 +124,18 @@ def results(token):
 
 @app.route('/filter/<token>', methods=['GET', 'POST'])
 def filter(token):
-    print(session[token])
+    bounds = []
+    for file in session[token]:
+        print(file)
+        cursor, conn = db_actions.set_cursor(file[0])
+        cursor = db_actions.clean_up(cursor, False, False)
 
+        print(db_actions.documentBounds(cursor))
+        [start, end] = db_actions.documentBounds(cursor)
+        bounds.append(start)
+        bounds.append(end)
 
-
-
-
+    return render_template('date-select.html', file_bounds=bounds, token=token)
 
 @app.route('/file_analysis/<token>', methods=['GET', 'POST'])
 def file_analysis(token):
